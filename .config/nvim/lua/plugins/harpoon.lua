a25a0e37e9382c40c1c8adddb4553448d9c61542
Matt Robinson <matthewrobinsondev@gmail.com>
2024-02-11 22:46:38 +0000

    Update keymaps for harpoon

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -5,23 +5,29 @@ return {
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
 
-		vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "Harpoon Add" })
+		vim.keymap.set("n", "<leader>ha", function()
+			mark.add_file()
+		end, { desc = "Add" })
 
-		vim.keymap.set("n", "<leader>hr", mark.rm_file, { desc = "Harpoon Remove" })
+		vim.keymap.set("n", "<leader>hr", function()
+			mark.rm_file()
+		end, { desc = "Remove" })
 
-		vim.keymap.set("n", "<leader>ho", ui.toggle_quick_menu)
+		vim.keymap.set("n", "<leader>ho", function()
+			ui.toggle_quick_menu()
+		end, { desc = "Toggle" })
 
 		vim.keymap.set("n", "<leader>1", function()
 			ui.nav_file(1)
-		end)
+		end, { desc = "Switch to harpoon 2" })
 		vim.keymap.set("n", "<leader>2", function()
 			ui.nav_file(2)
-		end)
+		end, { desc = "Switch to harpoon 3" })
 		vim.keymap.set("n", "<leader>3", function()
 			ui.nav_file(3)
-		end)
+		end, { desc = "Switch to harpoon 4" })
 		vim.keymap.set("n", "<leader>4", function()
 			ui.nav_file(4)
-		end)
+		end, { desc = "Switch to harpoon 5" })
 	end,
 }
