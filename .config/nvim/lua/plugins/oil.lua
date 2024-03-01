24ed9b6253fbaba4c9bcca46a27273429e270048
Matt Robinson <matthew.robinson@sykescottages.co.uk>
2024-03-01 14:51:58 +0000

    Add obsidian and oil tweaks for hidden files

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,4 +1,31 @@
 return {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = {
+    columns = {
+      'icon',
+      'permissions',
+      -- "size",
+      -- "mtime",
+    },
+    view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+    },
+    -- Configuration for the floating window in oil.open_float
+    float = {
+      -- Padding around the floating window
+      padding = 2,
+      max_width = 0,
+      max_height = 0,
+      border = 'rounded',
+      win_options = {
+        winblend = 0,
+      },
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      override = function(conf)
+        return conf
+      end,
+    },
+  },
 }
