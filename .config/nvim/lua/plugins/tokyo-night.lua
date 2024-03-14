641d13631369b7dc9f60c765231383011308f54e
Matt Robinson <matthewrobinsondev@gmail.com>
2024-03-14 14:59:59 +0000

    Updated colour scheme

--- .config/nvim/lua/plugins/tokyo-night.lua
+++ .config/nvim/lua/plugins/tokyo-night.lua
@@ -4,11 +4,11 @@ return { -- You can easily change to a different colorscheme.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
   'folke/tokyonight.nvim',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  lazy = true, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     -- Load the colorscheme here
-    vim.cmd.colorscheme 'tokyonight-night'
+    -- vim.cmd.colorscheme 'tokyonight-night'
 
     -- You can configure highlights by doing something like
     vim.cmd.hi 'Comment gui=none'
