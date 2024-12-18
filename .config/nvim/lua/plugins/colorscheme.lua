98d79cda5760eb9a8de3b312c1be55a14b1c2053
Matt Robinson <matthewrobinsondev@gmail.com>
2024-12-18 22:06:13 +0000

    grow up n pick a colour

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,53 +2,45 @@ return {
   {
     'projekt0n/github-nvim-theme',
     config = function()
-      vim.cmd.colorscheme 'github_dark_default'
+      -- vim.cmd.colorscheme 'github_dark_default'
     end,
   },
   {
     'catppuccin/nvim',
     lazy = false,
-    -- config = function()
-    --   require('catppuccin').setup {
-    --     integrations = {
-    --       cmp = true,
-    --       gitsigns = true,
-    --       harpoon = true,
-    --       illuminate = true,
-    --       indent_blankline = {
-    --         enabled = false,
-    --         scope_color = 'sapphire',
-    --         colored_indent_levels = false,
-    --       },
-    --       mason = true,
-    --       native_lsp = { enabled = true },
-    --       notify = true,
-    --       nvimtree = true,
-    --       neotree = true,
-    --       symbols_outline = true,
-    --       telescope = true,
-    --       treesitter = true,
-    --       treesitter_context = true,
-    --     },
-    --   }
-    --
-    --   -- vim.cmd.colorscheme 'catppuccin'
-    -- end,
+    config = function()
+      require('catppuccin').setup {
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          harpoon = true,
+          illuminate = true,
+          indent_blankline = {
+            enabled = false,
+            scope_color = 'sapphire',
+            colored_indent_levels = false,
+          },
+          mason = true,
+          native_lsp = { enabled = true },
+          notify = true,
+          nvimtree = true,
+          neotree = true,
+          symbols_outline = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+        },
+      }
+
+      vim.cmd.colorscheme 'catppuccin'
+    end,
   },
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
+  {
     'folke/tokyonight.nvim',
     lazy = true, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- Load the colorscheme here
       -- vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
     end,
   },
   {
@@ -69,6 +61,12 @@ return {
       }
 
       -- vim.cmd.colorscheme 'rose-pine-moon'
+    end,
+  },
+  {
+    'Mofiqul/dracula.nvim',
+    config = function()
+      -- vim.cmd.colorscheme 'dracula'
     end,
   },
 }
